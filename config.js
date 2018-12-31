bf33d5424c53e6a4c093342a6f1c5d34b397065f
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2018-12-31 11:01:33 +0100

    First draft of API: databases interactions and sql scheme defined.

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mysql: {
+        host: 'localhost',
+        user: 'tfjm-signin',
+        password: 'FYS7B0DVyO7K6Rvb',
+        database: 'tfjm-signin',
+    }
+};
--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,17 @@
-let login = function (req, res) {
-    res.status(200).end();
+const Participant = require('./model/Participant');
+
+let login = async function login(req, res) {
+    Participant.getAll()
+        .then(
+            rows => {
+                res.write("Participants : ", rows.toString());
+                res.end();
+            })
+        .catch(
+            err => {
+                res.write("Error : ", err.toString());
+                res.end();
+            });
 };
 
 
