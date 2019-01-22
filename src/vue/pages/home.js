e4a0976e30d081c16ff97fa4653a3d138b52710c
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2019-01-22 12:26:07 +0100

    Removing vue.

--- src/vue/pages/home.js
+++
@@ -1,4 +0,0 @@
-import Vue from "vue";
-import Home from '../components/home.vue';
-
-var app = new Vue(Home).$mount('#app');
--- src/vue/pages/login.js
+++
@@ -1,4 +0,0 @@
-import Vue from "vue";
-import Login from '../components/login.vue';
-
-var app = new Vue(Login).$mount('#app');
